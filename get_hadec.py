dafdf76c07e257681f682db4aa3e6f73d1e22a22
Leon Oostrum <leonoostrum@gmail.com>
2019-11-13 13:55:30 +0100

    Add tool to calculate hadec from a yaml file

---
+++ get_hadec.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import yaml
+import numpy as np
+import astropy.units as u
+from astropy.time import Time, TimeDelta
+
+from convert import ra_to_ha, ha_to_proj
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', required=True, help="Yaml config file")
+
+    args = parser.parse_args()
+
+    # source parameters
+    with open(args.file, 'r') as f:
+        params = yaml.load(f, Loader=yaml.SafeLoader)
+
+        tarr = Time(params['tstart']) + TimeDelta(params['t_in_obs'], format='sec')
+
+    if params['drift']:
+        # read hour angles
+        CB_best_center = SkyCoord(params['ha_best_cb'], params['dec_best_cb'], unit=u.deg, frame='icrs')
+        CB00_center = SkyCoord(params['ha_cb00'], params['dec_cb00'], unit=u.deg, frame='icrs')
+    else:
+        # read RA, Dec, convert to WSRT HA Dec
+        CB_best_center = ra_to_ha(params['ra_best_cb']*u.deg, params['dec_best_cb']*u.deg, tarr)
+        CB00_center = ra_to_ha(params['ra_cb00']*u.deg, params['dec_cb00']*u.deg, tarr)
+
+    posang_best = ha_to_proj(CB_best_center.ra, CB_best_center.dec)
+    posang_00 = ha_to_proj(CB00_center.ra, CB00_center.dec)
+
+    print("Best CB apparent HA, Dec, posang: ({:.5f}, {:.5f}), {:.5f}".format(CB_best_center.ra, CB_best_center.dec, posang_best))
+    print("CB00 apparent HA, Dec, posang: ({:.5f}, {:.5f}), {:.5f}".format(CB00_center.ra, CB00_center.dec, posang_00))

b087929c84ea6ec625f50c2b583aa6d997b3d740
Leon Oostrum <leonoostrum@gmail.com>
2020-08-12 12:51:05 +0200

    Turn into proper package; Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='arts_localisation',
+      version='0.1',
+      description='Localisation of ARTS transients',
+      url='http://github.com/loostrum/arts-localisation',
+      author='Leon Oostrum',
+      author_email='oostrum@astron.nl',
+      license='GPLv3',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=['numpy',
+                        'astropy<3',
+                        'pyyaml',
+                        'scipy',
+                        'matplotlib'],
+      include_package_data=True,
+      #entry_points={'console_scripts': ['darc=darc.control:main',
+      #                                  'darc_service=darc.darc_master:main']},
+      #scripts=['bin/darc_start_all_services',
+      #         'bin/darc_stop_all_services',
+      #         'bin/darc_start_master',
+      #         'bin/darc_stop_master',
+      #         'bin/darc_kill_all'])
+      )

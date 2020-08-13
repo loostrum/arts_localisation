fe2b70d4acd12247a59a6beeded6c0a510c30712
Leon Oostrum <leonoostrum@gmail.com>
2020-08-13 10:33:41 +0200

    Add localisation settings to yaml parser, add template yaml file

--- arts_localisation/config_parser.py
+++ arts_localisation/config_parser.py
@@ -3,6 +3,7 @@
 import logging
 
 import yaml
+import numpy as np
 import astropy.units as u
 from astropy.time import Time, TimeDelta
 
@@ -24,62 +25,65 @@ def parse_yaml(fname):
     with open(fname, 'r') as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
 
-    # read global settings, if present
-    source_coord = None
-    excluded_dishes = []
+    # read global settings
+    assert 'global' in config.keys(), 'Global config missing'
+
+    # check if required keys are present
+    for key in ('ra', 'dec', 'size', 'resolution', 'cb_model', 'fmin_data', 'bandwidth'):
+        assert key in config['global'].keys(), 'Key missing: {}'.format(key)
+
+    # check if frequency range to use is given, else set such that full range is used
+    if 'fmin' not in config['global'].keys():
+        config['global']['fmin'] = 0
+    if 'fmax' not in config['global'].keys():
+        config['global']['fmax'] = np.inf
+
+    # check for known source coordinates
+    try:
+        source_ra = config['global']['source_ra']
+        source_dec = config['global']['source_dec']
+        config['global']['source_coord'] = (source_ra * u.deg, source_dec * u.deg)
+    except KeyError:
+        logger.debug('No source coordinates found')
+
+    # check for S/N threshold
     snrmin = None
     try:
-        global_conf = config['global']
-        # check for known source coordinates
-        try:
-            source_ra = global_conf['source_ra']
-            source_dec = global_conf['source_dec']
-            source_coord = (source_ra * u.deg, source_dec * u.deg)
-        except KeyError:
-            logger.debug('No source coordinates found')
-
-
-        # check for S/N threshold
-        try:
-            snrmin = global_conf['snrmin']
-        except KeyError:
-            logger.debug('No S/N threshold found')
-
-        # delete global config from raw config so only bursts are left
-        del config['global']
-
+        snrmin = config['global']['snrmin']
     except KeyError:
-        logger.debug('No global config found')
-        pass
+        logger.debug('No S/N threshold found in global config')
 
     # the remaining config keys are individual bursts
-    for burst in config.keys():
+    bursts = [key for key in config.keys() if not key == 'global']
+    # there should be at least one burst
+    assert bursts, 'No bursts found'
+    for burst in bursts:
         # parameters of the burst
         # try to read arrival time, either directly or as start time plus ToA
         try:
-            tarr = Time(config[burst]['tarr'])
+            tarr = Time(config[burst]['tarr'], format='isot', scale='utc')
         except KeyError:
             try:
                 tarr = Time(config[burst]['start'], format='isot', scale='utc') + TimeDelta(config[burst]['toa'], format='sec')
-            except KeyError:
-                logger.error("Could not get arrival time for burst {} from either tarr ot tsart + toa".format(burst))
+            except Exception as e:
+                logger.error("Could not get arrival time for burst {} from either "
+                             "tarr ot tsart + toa ({})".format(burst, e))
                 raise
         # set arrival time
         config[burst]['tarr'] = tarr
 
         # check for excluded dishes
         try:
-            excluded_dishes = config[burst]['excluded_dishes']
             # convert to integers, RT2 = dish 0
-            excluded_dishes = [value - 2 for value in excluded_dishes]
+            config[burst]['excluded_dishes'] = [value - 2 for value in config[burst]['excluded_dishes']]
         except KeyError:
             logger.debug("No list of excluded dishes found")
 
         # check for telescope pointing
         pointing_coord = None
         try:
-            pointing_ra = global_conf['pointing_ra']
-            pointing_dec = global_conf['pointing_dec']
+            pointing_ra = config['global']['pointing_ra']
+            pointing_dec = config['global']['pointing_dec']
             pointing_coord = (pointing_ra * u.deg, pointing_dec * u.deg)
             logger.info("Telescope pointing found. Only use this option if ref_beam == 0.")
         except KeyError:
@@ -87,17 +91,20 @@ def parse_yaml(fname):
 
         # Verify S/N limit is present if not globally set
         if snrmin is None:
-            assert 'snrmin' in config[burst].keys()
+            assert 'snrmin' in config[burst].keys(), 'No global nor local S/N threshold found'
         # else set local S/N limit to global value
         config[burst]['snrmin'] = snrmin
 
-        # now check section for each compound beam, that start with CB
+        # now check section for each compound beam
         beams = [key for key in config[burst].keys() if key.upper().startswith('CB')]
+        # check reference CB is present and valid
+        assert 'reference_cb' in config[burst].keys(), 'Reference CB missing'
+        assert config[burst]['reference_cb'] in beams, 'Invalid reference CB: {}'.format(config[burst]['reference_cb'])
+        # there should be at least one beam
+        assert beams, 'No beams found for burst {}'.format(burst)
         for beam in beams:
-            # read keys
-            keys = config[burst][beam].keys()
-            # convert all to lowercase
-            keys = [k.lower() for k in keys]
+            # read keys in lowercase
+            keys = [key.lower() for key in config[burst][beam].keys()]
 
             # if ra, dec are given, use these
             if 'ra' in keys and 'dec' in keys:
@@ -125,4 +132,8 @@ def parse_yaml(fname):
                 logger.warning("No S/N array found for {} of burst {}, assuming it "
                                "is an upper limit beam".format(beam, burst))
 
-    return config, excluded_dishes, source_coord
+    return config
+
+
+if __name__ == '__main__':
+    print(parse_yaml('example.yaml'))

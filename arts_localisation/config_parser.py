6265a55459858baf7b9f85d4dbb85a2995060a77
Leon Oostrum <leonoostrum@gmail.com>
2020-08-18 10:19:12 +0200

    Update config file structure

--- arts_localisation/config_parser.py
+++ arts_localisation/config_parser.py
@@ -13,105 +13,120 @@ from arts_localisation.tools import cb_index_to_pointing, get_neighbours
 
 logger = logging.getLogger(__name__)
 
-REQUIRED_KEYS_SNR = ('snrmin', 'filterbank', 'cbs', 'neighbours', 'dm', 'window_load', 'window_zoom',
+REQUIRED_KEYS_GLOBAL = ('snrmin', 'fmin_data', 'bandwidth')
+
+REQUIRED_KEYS_SNR = ('filterbank', 'cbs', 'neighbours', 'dm', 'window_load', 'window_zoom',
                      'main_cb', 'main_sb')
-REQUIRED_KEYS_LOC = ('snrmin', 'ra', 'dec', 'size', 'resolution', 'cb_model', 'fmin_data', 'bandwidth')
+REQUIRED_KEYS_LOC = ('ra', 'dec', 'size', 'resolution', 'cb_model')
 
 
 def parse_yaml(fname, for_snr=False):
     """
     Parse a yaml file with settings for burst localisation
     :param str fname: Path to yaml config file
-    :param bool for_snr: Only load settings related to S/N determination, skip everything else
+    :param bool for_snr: Load settings for S/N determination, else load localisation settings
 
-    :return: config (dict), excluded_dishes (list, only if for_snr=False),
-             source_coord (SkyCoord or None, only if for_snr=False)
+    :return: config (dict)
     """
 
-    # read the file
+    # read the config file
     with open(fname) as f:
-        config = yaml.load(f, Loader=yaml.SafeLoader)
+        raw_config = yaml.load(f, Loader=yaml.SafeLoader)
 
-    # store path to the yaml file
+    # store path to the config file
     yaml_dir = os.path.dirname(os.path.abspath(fname))
 
     # read global settings
-    assert 'global' in config.keys(), 'Global config missing'
+    assert 'global' in raw_config.keys(), 'Global config missing'
+    conf_global = raw_config['global']
+
+    # verify all required keys are present
+    for key in REQUIRED_KEYS_GLOBAL:
+        assert key in conf_global.keys(), f'Global section key missing: {key}'
+
+    # set fmin/fmax if not specified in config
+    conf_global['fmin'] = conf_global.get('fmin', 0)
+    conf_global['fmax'] = conf_global.get('fmax', np.inf)
+
+    # copy the global config to the output config
+    config = conf_global.copy()
 
     # if for S/N determination, only load those settings
     if for_snr:
+        assert 'snr' in raw_config.keys(), 'S/N config missing'
+        conf_snr = raw_config['snr']
+        # verify all required S/N keys are present
         for key in REQUIRED_KEYS_SNR:
-            assert key in config['global'].keys(), f'Key missing: {key}'
-        snr_config = {}
-        for key in ('snrmin', 'dm', 'main_cb', 'main_sb', 'window_load', 'window_zoom'):
-            snr_config[key] = config['global'][key]
+            assert key in conf_snr.keys(), f'S/N section key missing: {key}'
 
-        # check if cb and tab keys are present in path
+        # check if cb and tab keys are present in filterbank path
         # first ensure two digits are used (which user may or may not have added)
-        path = config['global']['filterbank'].replace('{cb}', '{cb:02d}').replace('{tab}', '{tab:02d}')
+        path = conf_snr['filterbank'].replace('{cb}', '{cb:02d}').replace('{tab}', '{tab:02d}')
         assert '{cb:02d}' in path, '{cb} missing from path'
         assert '{tab:02d}' in path, '{tab} missing from path'
         # if a relative path, it is relative to the location of the yaml file
-        if not os.path.isfile(path.format(cb=snr_config['main_cb'], tab=0)):
+        if not os.path.isfile(path.format(cb=conf_snr['main_cb'], tab=0)):
             path = os.path.join(yaml_dir, path)
-        snr_config['filterbank'] = path
+        conf_snr['filterbank'] = path
 
         # check CB list is not empty
-        assert config['global']['cbs'], 'CB list cannot be empty'
+        assert conf_snr['cbs'], 'CB list cannot be empty'
         # if neighbours is True, add neighbouring beams
-        if config['global']['neighbours']:
-            snr_config['cbs'] = get_neighbours(config['global']['cbs'])
-        else:
-            snr_config['cbs'] = config['global']['cbs']
+        if conf_snr['neighbours']:
+            conf_snr['cbs'] = get_neighbours(conf_snr['cbs'])
 
-        return snr_config
+        # add the S/N settings to the global settings
+        config.update(conf_snr)
+        # Done for S/N mode
+        return config
 
     # localisation mode
+    assert 'localisation' in raw_config.keys(), 'Localisation config missing'
+    conf_loc = raw_config['localisation']
     # check if required keys are present
     for key in REQUIRED_KEYS_LOC:
-        assert key in config['global'].keys(), f'Key missing: {key}'
-
-    # check if frequency range to use is given, else set such that full range is used
-    if 'fmin' not in config['global'].keys():
-        config['global']['fmin'] = 0
-    if 'fmax' not in config['global'].keys():
-        config['global']['fmax'] = np.inf
+        assert key in conf_loc.keys(), f'Localisation section key missing: {key}'
 
     # check for known source coordinates
     source_coord = None
     try:
-        source_ra = config['global']['source_ra']
-        source_dec = config['global']['source_dec']
+        source_ra = conf_loc['source_ra']
+        source_dec = conf_loc['source_dec']
         source_coord = (source_ra * u.deg, source_dec * u.deg)
     except KeyError:
         logger.debug('No source coordinates found')
-    config['global']['source_coord'] = source_coord
+    conf_loc['source_coord'] = source_coord
+
+    # copy the localisation settings to the global config
+    config.update(conf_loc)
 
     # the remaining config keys are individual bursts
-    bursts = [key for key in config.keys() if not key == 'global']
+    bursts = [key for key in raw_config.keys() if key not in ['global', 'snr', 'localisation']]
     # there should be at least one burst
     assert bursts, 'No bursts found'
     # store list of bursts
     config['bursts'] = bursts
     for burst in bursts:
+        conf_burst = raw_config[burst]
         # parameters of the burst
         # try to read arrival time, either directly or as start time plus ToA
         try:
-            tarr = Time(config[burst]['tarr'], format='isot', scale='utc')
+            tarr = Time(conf_burst['tarr'], format='isot', scale='utc')
         except KeyError:
             try:
-                tarr = Time(config[burst]['start'], format='isot', scale='utc') + TimeDelta(config[burst]['toa'], format='sec')
+                tarr = Time(conf_burst['start'], format='isot', scale='utc') + \
+                       TimeDelta(conf_burst['toa'], format='sec')
             except Exception as e:
                 logger.error("Could not get arrival time for burst {} from either "
                              "tarr ot tsart + toa ({})".format(burst, e))
                 raise
         # set arrival time
-        config[burst]['tarr'] = tarr
+        conf_burst['tarr'] = tarr
 
         # check for excluded dishes
         try:
             # convert to integers, RT2 = dish 0
-            config[burst]['excluded_dishes'] = [value - 2 for value in config[burst]['excluded_dishes']]
+            conf_burst['excluded_dishes'] = [value - 2 for value in conf_burst['excluded_dishes']]
         except KeyError:
             logger.debug("No list of excluded dishes found")
 
@@ -126,37 +141,37 @@ def parse_yaml(fname, for_snr=False):
             logging.debug("No telescope pointing found")
 
         # now check section for each compound beam
-        beams = [key for key in config[burst].keys() if key.upper().startswith('CB')]
+        beams = [key for key in conf_burst.keys() if key.upper().startswith('CB')]
         # there should be at least one beam
         assert beams, f'No beams found for burst {burst}'
         # check reference CB is present and valid
-        assert 'reference_cb' in config[burst].keys(), 'Reference CB missing'
-        assert config[burst]['reference_cb'] in beams, 'Invalid reference CB: {}'.format(config[burst]['reference_cb'])
+        assert 'reference_cb' in conf_burst.keys(), 'Reference CB missing'
+        assert conf_burst['reference_cb'] in beams, 'Invalid reference CB: {}'.format(conf_burst['reference_cb'])
         # ensure reference CB is first in list
-        beams.remove(config[burst]['reference_cb'])
-        beams.insert(0, config[burst]['reference_cb'])
+        beams.remove(conf_burst['reference_cb'])
+        beams.insert(0, conf_burst['reference_cb'])
         # store list of beams
-        config[burst]['beams'] = beams
+        conf_burst['beams'] = beams
         for beam in beams:
             # read keys in lowercase
-            keys = [key.lower() for key in config[burst][beam].keys()]
+            keys = [key.lower() for key in conf_burst[beam].keys()]
 
             # if ra, dec are given, use these
             if 'ra' in keys and 'dec' in keys:
-                cb_pointing = (config[burst][beam]['ra'] * u.deg, config[burst][beam]['dec'] * u.deg)
+                cb_pointing = (conf_burst[beam]['ra'] * u.deg, conf_burst[beam]['dec'] * u.deg)
                 # if pointing is also set, warn the user
                 if pointing_coord is not None:
                     logger.warning("CB RA, Dec given, but telescope pointing is also set. Using CB RA, Dec")
             else:
                 # telescope pointing must be set
                 if pointing_coord is None:
-                    logger.error("No telescope pointing and not RA, Dec set for this CB")
+                    logger.error("No telescope pointing and no RA, Dec set for this CB")
                     raise
                 # calculate ra, dec from CB offsets
                 cb_index = int(beam[2:])
                 cb_pointing = cb_index_to_pointing(cb_index, *pointing_coord)
             # store the pointing of this CB
-            config[burst][beam]['pointing'] = cb_pointing
+            conf_burst[beam]['pointing'] = cb_pointing
 
             # SEFD is optional, but give user a warning if it is not present as it is easy to overlook
             if 'sefd' not in keys:
@@ -164,12 +179,19 @@ def parse_yaml(fname, for_snr=False):
 
             # check for S/N array; absent means it is assumed to be an upper limit beam
             if 'snr_array' in keys:
+                snr_array_path = conf_burst[beam]['snr_array']
                 # if the file cannot be found, assume it is a relative path
-                if not os.path.isfile(config[burst][beam]['snr_array']):
-                    config[burst][beam]['snr_array'] = os.path.join(yaml_dir, config[burst][beam]['snr_array'])
+                if not os.path.isfile(snr_array_path):
+                    logger.debug('S/N array not found, trying to convert relative to absolute path')
+                    snr_array_path = os.path.join(yaml_dir, snr_array_path)
+                # if the file still cannot be found, give a warning
+                logger.warning(f'Cannot find S/N array file {snr_array_path}')
+                conf_burst['snr_array'] = snr_array_path
             else:
                 logger.info("No S/N array found for {} of burst {}, assuming it "
                             "is an upper limit beam".format(beam, burst))
+        # copy the burst config to the global config
+        config[burst] = conf_burst
 
     return config
 

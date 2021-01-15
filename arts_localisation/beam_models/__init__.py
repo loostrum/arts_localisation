5e36bab82f8ade245e3c19bd49d70c5f0e395cdd
Leon Oostrum <leonoostrum@gmail.com>
2021-01-15 16:34:30 +0100

    Add SB model that calculates single HA/Dec coordinate; make numba usage thread-safe

--- arts_localisation/beam_models/__init__.py
+++ arts_localisation/beam_models/__init__.py
@@ -4,6 +4,7 @@ from .beamformer import BeamFormer
 from .compound_beam import CompoundBeam
 from .sb_generator import SBGenerator
 from .simulate_sb_pattern import SBPattern
+from .sb_pattern_single import SBPatternSingle
 
 
-__all__ = ['BeamFormer', 'CompoundBeam', 'SBPattern', 'SBGenerator']
+__all__ = ['BeamFormer', 'CompoundBeam', 'SBPattern', 'SBPatternSingle', 'SBGenerator']

4995bdcf1024721ed839044bb3c2e02e62e4b036
Leon Oostrum <leonoostrum@gmail.com>
2021-01-18 17:03:46 +0100

    Load S/N data and create vector of initial guesses

--- arts_localisation/config_parser.py
+++ arts_localisation/config_parser.py
@@ -19,7 +19,8 @@ REQUIRED_KEYS_GLOBAL = ('snrmin', 'fmin_data', 'bandwidth')
 REQUIRED_KEYS_SNR = ('dm', 'window_load', 'window_zoom', 'width_max')
 REQUIRED_KEYS_SNR_BURST = ('main_sb', 'main_cb', 'filterbank', 'cbs', 'neighbours')
 REQUIRED_KEYS_LOC = ('dec', 'size', 'resolution', 'cb_model')
-REQUIRED_KEYS_LOC_MCMC = ('dec', 'cb_model')
+REQUIRED_KEYS_LOC_MCMC = ('dec', 'cb_model', 'guess_pointing_max_offset', 'guess_boresight_snr_range',
+                          'guess_beamwidth_max_offset', 'guess_snr_offset_max_offset')
 
 
 def parse_yaml(fname, for_snr=False, mcmc=False):
--- arts_localisation/run_localisation_mcmc.py
+++ arts_localisation/run_localisation_mcmc.py
@@ -7,6 +7,7 @@ import argparse
 import numpy as np
 from astropy.time import Time
 import astropy.units as u
+from astropy.coordinates import SkyCoord
 import emcee
 import matplotlib.pyplot as plt
 import corner
@@ -14,7 +15,7 @@ from schwimmbad import MPIPool
 from mpi4py import MPI
 
 from arts_localisation import tools
-from arts_localisation.constants import NSB
+from arts_localisation.constants import REF_FREQ, CB_HPBW, NSB
 from arts_localisation.beam_models import SBPatternSingle
 from arts_localisation.config_parser import load_config
 
@@ -70,15 +71,95 @@ class TestData:
         self.ha_cb, self.dec_cb = tools.radec_to_hadec(self.burst['ra_cb'], self.burst['dec_cb'], self.tarr)
 
 
-def set_guess_value(ndim, minval, maxval):
+def initialize_parameters(config, ndim, snr_data):
+    """
+    Create an array of initial guesses for each parameter:
+
+    #. RA (rad)
+    #. Dec (rad)
+    #. Boresight S/N (per burst)
+    #. Primary beam width RA (rad, per CB of each burst)
+    #. Primary beam width Dec (rad, per CB of each burst)
+    #. S/N offset (per CB of each burst)
+
+    :param dict config: config from .yaml file
+    :param int ndim: number of guesses to use for each parameter
+    :param np.array snr_data: Max S/N for each burst
+    :return: guesses (array)
+    """
+    # global parameters: ra and dec
+    ra0 = (config['ra'] * u.deg).to(u.rad).value
+    dec0 = (config['dec'] * u.deg).to(u.rad).value
+
+    max_offset = (config['guess_pointing_max_offset'] * u.arcmin).to(u.rad).value
+    guess_dec = dec0 + get_guess_values(ndim, -max_offset, max_offset)
+    guess_ra = ra0 + get_guess_values(ndim, -max_offset, max_offset) / np.cos(guess_dec)
+
+    # parameters per burst: boresight S/N
+    nburst = len(config['bursts'])
+    # TODO: need access to measured S/N here
+    minval, maxval = np.array(config['guess_boresight_snr_range'])
+    guess_boresight_snr = get_guess_values((ndim, nburst), minval, maxval) * snr_data[None, :]
+
+    # parameters per CB per burst
+    # find out how many parameters to generate
+    num_guess = 0
+    for burst in config['bursts']:
+        nbeam = len(config[burst]['beams'])
+        num_guess += nbeam
+
+    # beam width in RA and Dec
+    central_freq = int(np.round(config['fmin_data'] + config['bandwidth'] / 2)) * u.MHz
+    beam_width0 = (CB_HPBW * REF_FREQ / central_freq).to(u.rad).value
+    max_offset = (config['guess_beamwidth_max_offset'] * u.arcmin).to(u.rad).value
+    guess_width_ra = beam_width0 + get_guess_values((ndim, num_guess), -max_offset, max_offset)
+    guess_width_dec = beam_width0 + get_guess_values((ndim, num_guess), -max_offset, max_offset)
+
+    # S/N offset
+    guess_snr_offset = get_guess_values((ndim, num_guess), 0, config['guess_snr_offset_max_offset'])
+
+    # output shape must be (nwalker, nparam)
+    guesses = np.hstack([guess_ra[:, None], guess_dec[:, None], guess_boresight_snr,
+                         guess_width_ra, guess_width_dec, guess_snr_offset])
+
+    return guesses
+
+
+def get_guess_values(ndim, minval, maxval):
     """
     Generate a set of guess parameters
 
-    :param int ndim: dimensions of guess vector
+    :param tuple,int ndim: dimensions of guess vector
     :param float minval: minimum value
     :param float maxval: maximum value
     """
     return (maxval - minval) * np.random.random(ndim) + minval
+
+
+def load_snr_data(config):
+    """
+    Load S/N arrays from disk
+
+    :param dict config: config from .yaml file
+    :return: S/N array for each CB of each burst (array), number of CBs per burst (list)
+    """
+    numcb_per_burst = []
+    data = []
+    for burst in config['bursts']:
+        nbeam = len(config[burst]['beams'])
+        burst_data = np.zeros((nbeam, NSB), dtype=float)
+        numcb_per_burst.append(nbeam)
+        for ind, beam in enumerate(config[burst]['beams']):
+            # load the S/N array
+            fname = config[burst][beam]['snr_array']
+            snr_data = np.loadtxt(fname, ndmin=2)
+            sb_det, snr_det = snr_data.T
+            if len(snr_det) != NSB:
+                logging.error(f"Number of S/N values in {fname} does not equal number of SBs ({NSB}), "
+                              f"please re-run arts_calc_snr to get a value in each SB")
+            burst_data[ind] = snr_det
+        data.append(burst_data)
+    return data, numcb_per_burst
 
 
 def main():
@@ -91,6 +172,8 @@ def main():
     if mpi_rank == 0:
         parser = ArgumentParser()
         parser.add_argument('--config', required=True, help='Input yaml config')
+        parser.add_argument('--nwalker', type=int, help='Number of MCMC walkers')
+        parser.add_argument('--nstep', type=int, help='Number of MCMC steps')
         parser.add_argument('--load', help='Path to .h5 file from previous run. Will load this file instead of'
                                            'executing another MCMC run')
         parser.add_argument('--output_folder', help='Output folder '
@@ -131,13 +214,42 @@ def main():
         logging.error(f"Need more than one process to be able to execute MCMC run. Run this "
                       f"script with mpirun -np <nproc> {os.path.basename(__file__)}")
         sys.exit()
+    elif args.load is None and (args.nstep is None or args.nwalker is None):
+        if mpi_rank == 0:
+            logging.error("Specify nwalker and nstep when doing an MCMC run")
+            sys.exit()
+        else:
+            sys.exit()
 
     # set matplotlib backend to non-interactive if only saving plots
     if args.save_plots and not args.show_plots:
         plt.switch_backend('pdf')
 
+    # get output prefix from yaml file name and output folder
+    if args.output_folder is None:
+        # default output folder is same folder as .yaml file plus "localisation"
+        args.output_folder = os.path.join(os.path.dirname(os.path.abspath(args.config)), 'localisation')
+    if mpi_rank == 0:
+        tools.makedirs(args.output_folder)
+    # output prefix also contains the yaml filename without extension
+    output_prefix = os.path.join(args.output_folder, os.path.basename(args.config).replace('.yaml', ''))
+
     # load config
-    config = load_config(args, mcmc=True)
+    config = None
+    if mpi_rank == 0:
+        config = load_config(args, mcmc=True)
+    config = mpi_comm.bcast(config, root=0)
+
+    # load S/N arrays
+    snr_data = None
+    if mpi_rank == 0:
+        snr_data = load_snr_data(config)
+    snr_data, numcb_per_burst = mpi_comm.bcast(snr_data, root=0)
+
+    # create initial parameter vector
+    # get max S/N of each burst
+    max_snr_per_burst = np.array([burst.max() for burst in snr_data])
+    initial_guess = initialize_parameters(config, args.nwalker, max_snr_per_burst)
 
 
 if __name__ == '__main__':
